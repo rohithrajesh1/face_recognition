fcb9ef2d6dbc6090093b058fa9cc8ad5a0bd4c0c
rohithrajesh1 <48838176+rohithrajesh1@users.noreply.github.com>
2020-05-05 23:18:17 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -48,13 +48,20 @@ app.get('/',(req,res)=>{
 })
 
 app.post('/signin',(req,res)=>{
-    if(req.body.email===database.users[0].email && req.body.password===database.users[0].password){
-        res.json('success');
-    }
-    else{
-        res.status(400).json("error logging in")
-    }
-
+    knex.select('email','hash').from('login')
+    .where('email','=',req.body.email)
+    .then(data=>{
+       const isValid= bcrypt.compareSync(req.body.password, data[0].hash);
+       if(isValid){
+        return knex.select('*').from('users')
+        .where('email','=',req.body.email)
+        .then(user=>{
+            res.json(user[0])
+        })
+        .catch(err=>res.status(400).json('unable to get user'))
+       }
+    })
+    .catch(err=>res.status(400).json('unable to get user'))
 })
 
 app.post('/register',(req,res)=>{
