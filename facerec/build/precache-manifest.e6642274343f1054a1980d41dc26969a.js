72f6a0b309083debb295fcb604b1db525a3e4c59
rohithrajesh1 <48838176+rohithrajesh1@users.noreply.github.com>
2020-05-07 15:40:35 +0530

    deploy

---
+++ facerec/build/precache-manifest.e6642274343f1054a1980d41dc26969a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed6d2f8d2bb4aab76c7eced6552c094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f25268618872ac9e5f3d",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "603b227dd9260b136e5e",
+    "url": "/static/css/main.3be881b5.chunk.css"
+  },
+  {
+    "revision": "f25268618872ac9e5f3d",
+    "url": "/static/js/2.8017ffc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8017ffc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603b227dd9260b136e5e",
+    "url": "/static/js/main.ab803127.chunk.js"
+  },
+  {
+    "revision": "18180198ee2bada576ca",
+    "url": "/static/js/runtime-main.2eb92bac.js"
+  }
+]);
